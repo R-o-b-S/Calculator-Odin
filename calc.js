0d6c8236575439dfd68740a13dbf73b42c68fd28
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-15 18:53:19 +0200

    added operate function that takes 2 numbers and 1 operator and execute the right operation

--- calc.js
+++ calc.js
@@ -1,19 +1,26 @@
-function add (a,b) {
+function add (a,b) { //adds 2 numbers
     const c = a+b;
     return c;
 }
 
-function sub (a,b) {
+function sub (a,b) { //subtruct 2 numbers
     const c = a-b;
     return c;
 }
 
-function multi (a,b) {
+function multi (a,b) { //multiply 2 numbers
     const c = a*b;
     return c;
 }
 
-function div (a,b) {
+function div (a,b) { //divide 2 numbers
     const c = a/b;
     return c;
+}
+
+function operate (a,o,b) { //takes 2 numbers (a,b) and an operator (o) and calls operation function on the numbers
+    if (o === "+") {add (a,b);}
+    else if (o === "-") {sub (a,b);}
+    else if (o === "*") {multi (a,b);}
+    else if (o === "/") {div (a,b);}
 }
