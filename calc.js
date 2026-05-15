ea87256da6a71466fd8980981675b7b8f1f35ed6
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-15 18:44:23 +0200

    added basic operation function add for +, sub for -, multi for *, div for /

---
+++ calc.js
@@ -0,0 +1,19 @@
+function add (a,b) {
+    const c = a+b;
+    return c;
+}
+
+function sub (a,b) {
+    const c = a-b;
+    return c;
+}
+
+function multi (a,b) {
+    const c = a*b;
+    return c;
+}
+
+function div (a,b) {
+    const c = a/b;
+    return c;
+}
